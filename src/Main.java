8f235e817ea29c7c4b038a43c40d3d2a92e21d97
Luc8020 <roveratiluca30@gmail.com>
2024-08-23 12:54:19 +0200

    TcTacToe

--- src/Main.java
+++ src/Main.java
@@ -5,60 +5,76 @@ public class Main {
         game();
     }
 
-    static void game()
-    {
+    static void game() {
         Scanner scanner = new Scanner(System.in);
-        printfield(scanner.nextInt(), scanner.nextInt(), "x");
-    }
-    static void printfield(int x, int y, String player)
-    {
-        int runs = 0;
-        String[][] field = new String[3][3];
-        if(runs==0)
-        {
-            for(int i=0;i<field.length;i++)
-            {
-                for(int j=0;j<field[2].length;j++)
-                {
-                    field[i][j] = "#";
+        String[][] field = initializeField();
+        String currentPlayer = "x";
+        while (true) {
+            System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            if (isValidMove(x, y, field)) {
+                field[x - 1][y - 1] = currentPlayer;
+                printField(field);
+                movehistory(x, y, currentPlayer);
+                if (checkWin(field, currentPlayer)) {
+                    System.out.println("Player " + currentPlayer + " wins!");
+                    break;
                 }
+                currentPlayer = currentPlayer.equals("x") ? "o" : "x"; // Switch player
+            } else {
+                System.out.println("Invalid move. Try again.");
             }
-            runs++;
-        }
-        try {
-            field[x-1][y-1]=player;
-        }
-        catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException)
-        {
-            System.out.println("Bitte gültiges Feld eingeben (1 1-3 3)");
-            Scanner scanner = new Scanner(System.in);
-            printfield(scanner.nextInt(), scanner.nextInt(), player);
-        }
-
-        for(int i=0;i<field.length;i++)
-        {
-            for(int j=0;j<field[2].length;j++)
-            {
-                System.out.print(field[i][j]);
-            }
-            System.out.println("");
         }
     }
-    static String movehistory(int x, int y, String currentplayer)
-    {
-        int runs = 0;
-        int[] X = new int[8];
-        int[] O = new int[8];
-        if(runs==0)
-        {
-            for(int i = 0; i<X.length; i++)
-            {
-                X[i]=0;
-                O[i]=0;
+
+    static String[][] initializeField() {
+        String[][] field = new String[3][3];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                field[i][j] = "#";
             }
-            runs++;
         }
-        return (null);
+        return field;
     }
 
+    static void printField(String[][] field) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                System.out.print(field[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static boolean isValidMove(int x, int y, String[][] field) {
+        if (x < 1 || x > 3 || y < 1 || y > 3) {
+            return false;
+        }
+        return field[x - 1][y - 1].equals("#");
+    }
+
+    static void movehistory(int x, int y, String currentPlayer) {
+        // Here you could store the history of moves if needed
+        // This function is just a placeholder in this context
+    }
+
+    static boolean checkWin(String[][] field, String player) {
+        // Check rows, columns, and diagonals for a win
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(player) && field[i][1].equals(player) && field[i][2].equals(player)) {
+                return true;
+            }
+            if (field[0][i].equals(player) && field[1][i].equals(player) && field[2][i].equals(player)) {
+                return true;
+            }
+        }
+        if (field[0][0].equals(player) && field[1][1].equals(player) && field[2][2].equals(player)) {
+            return true;
+        }
+        if (field[0][2].equals(player) && field[1][1].equals(player) && field[2][0].equals(player)) {
+            return true;
+        }
+        return false;
+    }
 }
